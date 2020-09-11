c6218db837d19da6eafcd5f8c3d85a4b803f2ac6
Jack Ho <j345274@gmail.com>
2020-09-11 15:57:26 +0800

    Ensure enough memory space when insert node

--- queue.c
+++ queue.c
@@ -48,12 +48,32 @@ void q_free(queue_t *q)
 bool q_insert_head(queue_t *q, char *s)
 {
     list_ele_t *newh;
+    char *newstr;
+    int len = strlen(s);
     /* TODO: What should you do if the q is NULL? */
+    if (!q) {
+        return false;
+    }
     newh = malloc(sizeof(list_ele_t));
+    while (!newh) {
+        newh = malloc(sizeof(list_ele_t));
+    }
+    newstr = malloc(sizeof(char) * len + 1);
+    while (!newstr) {
+        newstr = malloc(sizeof(char) * len + 1);
+    }
+    for (int i = 0; i < len; i++) {
+        *(newstr + i) = *(s + i);
+    }
+    *(newstr + len) = '\0';
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
     newh->next = q->head;
+    newh->value = newstr;
     q->head = newh;
+    if (!(q->tail))
+        q->tail = newh;
+    (q->size)++;
     return true;
 }
 
@@ -69,7 +89,34 @@ bool q_insert_tail(queue_t *q, char *s)
     /* TODO: You need to write the complete code for this function */
     /* Remember: It should operate in O(1) time */
     /* TODO: Remove the above comment when you are about to implement. */
-    return false;
+    list_ele_t *newt;
+    char *newstr;
+    int len = strlen(s);
+    if (!q)
+        return false;
+    newt = malloc(sizeof(list_ele_t));
+    while (!newt) {
+        newt = malloc(sizeof(list_ele_t));
+    }
+    newstr = malloc(sizeof(char) * len + 1);
+    while (!newstr) {
+        newstr = malloc(sizeof(char) * len + 1);
+    }
+    for (int i = 0; i < len; i++) {
+        *(newstr + i) = *(s + i);
+    }
+    *(newstr + len) = '\0';
+    newt->next = NULL;
+    newt->value = newstr;
+    if (!q->tail) {
+        q->head = newt;
+        q->tail = newt;
+    } else {
+        q->tail->next = newt;
+        q->tail = newt;
+    }
+    (q->size)++;
+    return true;
 }
 
 /*
